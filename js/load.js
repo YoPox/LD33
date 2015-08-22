383bfd7a15b03d15580c6da81cab5b208cc10a24
YoPox <yopoxdev@gmail.com>
2015-08-22 15:59:42 +0200

    Bitmap font, other animations

--- js/load.js
+++ js/load.js
@@ -1,47 +1,72 @@
+// SPRITES
+var rect;
+var text;
+var logo;
+
+// STUFF
 var titleEmitter;
+
 var loadState = {
 
   preload: function() {
 
     game.load.image("logo", "../assets/logo.png");
-    game.load.image("boot", "../assets/working/boot.png");
     game.load.image("title", "../assets/working/title.png");
     game.load.image("part", "../assets/bluePart.png");
     game.load.image("part2", "../assets/redPart.png");
+    game.load.image("phaser", "../assets/phaser.png");
+    game.load.bitmapFont('Munro', '../assets/font/Munro.png', '../assets/font/Munro.fnt');
 
   },
 
   create: function() {
 
-    var title = game.add.sprite(0, 0, "boot");
+    game.stage.backgroundColor = "#2B3D73";
 
+    // WHITE BACKGROUND
+    var bmd = game.add.bitmapData(1024, 576);
+    bmd.ctx.beginPath();
+    bmd.ctx.rect(0, 0, 1024, 576);
+    bmd.ctx.fillStyle = '#ffffff';
+    bmd.ctx.fill();
+    rect = game.add.sprite(0, 0, bmd);
+
+    // ELLECTRON.COM TEXT
+    text = game.add.bitmapText(game.width / 2,  3 * game.height / 4, "Munro", "eLLectron.com", 75);
+    text.anchor.set(0.5, 0.5);
+    text.smoothed = false;
+
+    // PARTICLES
     titleEmitter = game.add.emitter(game.width / 2 + 1, game.height / 2 - 65, 1000);
     titleEmitter.makeParticles(['part', 'part2']);
     titleEmitter.gravity = 0;
     titleEmitter.minParticleSpeed.setTo(-300, -300);
     titleEmitter.maxParticleSpeed.setTo(300, 300);
 
-    var logo = game.add.sprite(game.width / 2 - 128, game.height / 2 - 192, 'logo');
+    setTimeout(function () {
+      titleEmitter.start(false, 750, 10);
+    }, 750);
+
+    // ELLECTRON's LOGO
+    logo = game.add.sprite(game.width / 2 - 128, game.height / 2 - 192, 'logo');
     logo.scale.set(4);
     logo.smoothed = false;
     logo.alpha = 0;
     game.add.tween(logo).to( { alpha: 1 }, 1500, Phaser.Easing.Exponential.InOut, true);
 
-    setTimeout(function () {
-      titleEmitter.start(false, 750, 10);
-    }, 750);
-
-    game.stage.backgroundColor = "#2B3D73";
-
+    // FINAL FADE OUT
     setTimeout(function () {
       titleEmitter.on = false;
       game.add.tween(logo).to( { alpha: 0 }, 1500, Phaser.Easing.Exponential.OutIn, true);
-      game.add.tween(title).to( { alpha: 0 }, 1500, Phaser.Easing.Exponential.OutIn, true);
+      game.add.tween(rect).to( { alpha: 0 }, 1500, Phaser.Easing.Exponential.OutIn, true);
+      game.add.tween(text).to( { alpha: 0 }, 1500, Phaser.Easing.Exponential.OutIn, true);
     }, 2000);
 
+    // DESTROY SPRITES AND START TITLE SCREEN
     setTimeout(function () {
       logo.destroy();
-      title.destroy();
+      rect.destroy();
+      text.destroy();
       game.state.start("menu");
     }, 3500);
 
@@ -49,6 +74,7 @@ var loadState = {
 
   update: function() {
 
+    // PARTICLES FADE OUT EFFECT
     titleEmitter.forEachAlive(function(p){
   		p.alpha = p.lifespan / titleEmitter.lifespan;
   	});
--- js/menu.js
+++ js/menu.js
@@ -1,24 +1,68 @@
+// SPRITES
+var title;
+var phaser;
+var playText;
+var shareText;
+var selectionRect;
+
+// STUFF
+var moveInt;
+
 var menuState = {
 
   create: function() {
 
-    var title = game.add.sprite(game.width / 2 - 256, game.width / 2 - 256, 'title');
+    // LOGO
+    title = game.add.sprite(game.width / 2 - 256, game.height - 256, 'title');
     title.scale.set(4);
     title.smoothed = false;
     title.alpha = 0;
+    // ANIMATION
     game.add.tween(title).to( { alpha: 1 }, 2000, Phaser.Easing.Exponential.InOut, true);
     game.add.tween(title).to( { y: game.height / 2 - 256 }, 1500, Phaser.Easing.Exponential.InOut, true);
-
-    var moveInt = setInterval(function () {
+    // REGULAR MOVE
+    moveInt = setInterval(function () {
       game.add.tween(title).to( { y: title.y + 4 }, 1500, Phaser.Easing.Exponential.InOut, true);
       setTimeout(function () {
-        game.add.tween(title).to( { y: title.y - 4 }, 1500, Phaser.Easing.Exponential.OutIn, true);
+        game.add.tween(title).to( { y: title.y - 4 }, 1500, Phaser.Easing.Exponential.InOut, true);
       }, 1500);
     }, 3000);
+
+    // SELECTION RECTANGLE
+
+    // PLAY TEXT
+    playText = game.add.bitmapText(game.width / 2 - 64,  3 * game.height / 4 - 88, "Munro", "Play", 64);
+    playText.anchor.set(0.5, 0.5);
+    playText.smoothed = false;
+    playText.alpha = 0;
+    setTimeout(function () {
+      game.add.tween(playText).to( { alpha: 1 }, 1000, Phaser.Easing.Exponential.OutIn, true);
+      game.add.tween(playText).to( { x: game.width / 2 }, 750, Phaser.Easing.Exponential.InOut, true);
+    }, 900);
+
+    // SHARE TEXT
+    shareText = game.add.bitmapText(game.width / 2 + 64,  3 * game.height / 4 + 16, "Munro", "Share", 64);
+    shareText.anchor.set(0.5, 0.5);
+    shareText.smoothed = false;
+    shareText.alpha = 0;
+    setTimeout(function () {
+      game.add.tween(shareText).to( { alpha: 1 }, 1000, Phaser.Easing.Exponential.OutIn, true);
+      game.add.tween(shareText).to( { x: game.width / 2 }, 750, Phaser.Easing.Exponential.InOut, true);
+    }, 950);
+
+    // PHASER's LOGO
+    phaser = game.add.sprite(game.width / 2 - 52, game.height + 30, 'phaser');
+    phaser.alpha = 0;
+    setTimeout(function () {
+      game.add.tween(phaser).to( { alpha: 1 }, 1000, Phaser.Easing.Exponential.InOut, true);
+      game.add.tween(phaser).to( { y: game.height - 30 }, 750, Phaser.Easing.Exponential.InOut, true);
+    }, 1200);
 
   },
 
   start: function() {
+
+    clearInterval(moveInt);
 
     game.state.start("play");
 
